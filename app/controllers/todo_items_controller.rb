0050796775c26de56ad30e7fc5064de05e538275
sruthisarav <sruthisarav2@gmail.com>
2020-01-17 14:29:35 +0800

    created todo item controller and view

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,12 @@
+class TodoItemsController < ApplicationController
+  before_action :make_todo_list
+  def create
+    @todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
+    redirect_to @todo_list
+  end
+
+  private
+    def make_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+end
